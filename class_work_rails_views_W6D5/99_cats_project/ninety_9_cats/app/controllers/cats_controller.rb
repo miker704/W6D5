7f79dea90a2ea78af2d8e371e7e81f953032ce78
Vivian Chen <vnchen92@gmail.com>
2022-05-06 15:57:43 -0400

    Started 99 cats

---
+++ class_work_rails_views_W6D5/99_cats_project/ninety_9_cats/app/controllers/cats_controller.rb
@@ -0,0 +1,18 @@
+class CatsController < ApplicationController
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+    end
+
+    
+
+    private
+    def cat_params
+        params.require(:cats).permits(:birth_date, :color, :name, :sex, :description)
+    end
+end
--- class_work_rails_views_W6D5/99_cats_project/ninety_9_cats/db/seeds.rb
+++ class_work_rails_views_W6D5/99_cats_project/ninety_9_cats/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cat1 = Cat.create(
+    birth_date: '2015/08/09',
+    color: 'blue',
+    name: 'Blue',
+    sex: 'f',
+    description: 'I am blue.'
+)
+
+cat2 = Cat.create(
+    birth_date: '2015/11/01',
+    color: 'orange',
+    name: 'Orange',
+    sex: 'm',
+    description: 'I am orange.'
+)
